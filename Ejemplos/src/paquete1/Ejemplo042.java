c87179a9f353274855773cabbec76f139c07a64b
svalverde007 <sdvalverde2@utpl.edu.ec>
2025-12-10 12:10:11 -0500

    Practica 2

--- Ejemplos/src/paquete1/Ejemplo042.java
+++ Ejemplos/src/paquete1/Ejemplo042.java
@@ -9,26 +9,34 @@ package paquete1;
  *
  * @author reroes
  */
-public class Ejemplo04 {
+public class Ejemplo042 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // Declarar y generar arreglos en Java
-        
+
         // arreglo de tipo entero
         // int[] arreglo1 = new int[5];
+        int suma = 0;
         int[] arreglo1 = {1000, 1, 2, 2000, 300};
+        String acumulador = "";
         // System.out.println(arreglo1.length);
         // arreglo1[2] = 1000;
-        // sirve para definir el numero de elementos que va a tener arreglo
         for (int i = 0; i < arreglo1.length; i++) {
-            System.out.printf("Subíndice o Índice %d, con valor %d\n",
-                    i, arreglo1[i]);
+            if (arreglo1[i] >= 1000) {
+
+                suma = suma + arreglo1[i];
+                acumulador = String.format("%sSubíndice o Índice %d, "
+                        + "con valor %d\n",
+                        acumulador, i, arreglo1[i]);
+            }
         }
-        
-        
+        acumulador = String.format("%sLa suma total es: %d\n",
+                acumulador, suma);
+        System.out.printf("%s\n",
+                acumulador);
     }
 
 }
