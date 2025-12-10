2e1f7f01ac86d0419ed3c5ce71affa97a47fc680
svalverde007 <sdvalverde2@utpl.edu.ec>
2025-12-10 12:46:42 -0500

    Practica 6

--- Ejemplos/src/paquete1/Ejemplo061.java
+++ Ejemplos/src/paquete1/Ejemplo061.java
@@ -22,23 +22,37 @@ public class Ejemplo061 {
         String dato;
         // arreglo de tipo entero
         String[] arreglo1 = new String[5]; //  se guardaran nombres de personas
-        
+
         // Se asigna valores al arreglo, pidiendo valores por teclado
         // se usa un ciclo repetitivo
         for (int i = 0; i < arreglo1.length; i++) {
             System.out.println("Ingrese un valor para el arreglo");
             dato = entrada.nextLine();
+            String letra = dato.substring(0, 1);
+            letra = letra.toLowerCase();
+            switch (letra) {
+                case "a":
+                case "e":
+                case "i":
+                case "o":
+                case "u":
+                    arreglo1[i] = dato;
+                    break;
+                default:
+                    arreglo1[i] = "invalido";
+            }
             // se asigna la variable dato a la posición del arreglo
             // de acuerdo al valor de i
-            arreglo1[i] = dato;
+            
+
         }
-        
+
         // Se hace uso del ciclo repetitivo for para recorrer el arreglo
         for (int i = 0; i < arreglo1.length; i++) {
             System.out.printf("Subíndice o Índice %d, con valor %s\n",
                     i, arreglo1[i]);
         }
-        
+
     }
 
 }
