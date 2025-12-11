714bbdb8c2ce885a7f4a9d2d8f536e93f1d3589d
svalverde007 <sdvalverde2@utpl.edu.ec>
2025-12-11 11:21:52 -0500

    practica 111

--- Ejemplos/src/paquete1/Ejemplo11.java
+++ Ejemplos/src/paquete1/Ejemplo11.java
@@ -20,21 +20,21 @@ public class Ejemplo11 {
      */
     public static void main(String[] args) {
         // Declarar y generar arreglos en Java
-        
+
         // Se declara un arreglo de tipo double, llamado promedios
-        double [] promediosMateria1 = {8.1, 9, 9.5, 10, 9.9};
+        double[] promediosMateria1 = {8.1, 9, 9.5, 10, 9.9};
         double[] promediosMateria2 = {7.9, 9.2, 9.7, 9.8, 9.7};
         String[] estudiantes = {"Luis", "Ana", "Mónica", "José", "Juan"};
-              // promedioFinales = {8.0, 0.0, 0.0, 0.0, 0.0}
+        // promedioFinales = {8.0, 0.0, 0.0, 0.0, 0.0}
         double[] promediosFinales = new double[5];
-        
+
         double sumaNotas;
         double promedio;
-        
+
         String mensajeFinal = ""; // variable que acumula un reporte final
         // Se hace uso del ciclo repetitivo for para recorrer los arreglos
         // y presentar los valore de las posiciones
-        
+
         for (int i = 0; i < promediosFinales.length; i++) {
             // se suma el valor de la posición i del arreglo 
             // promediosMateria1 y promediosMateria2
@@ -44,18 +44,20 @@ public class Ejemplo11 {
             // se asigna a la posición i del arreglo promediosFinales
             promediosFinales[i] = promedio;
         }
-        
-        for (int i = 0; i < promediosMateria1.length; i++) { 
-            
+
+        for (int i = 0; i < promediosMateria1.length; i++) {
+
             mensajeFinal = String.format("%sEstudiante:%s - promedio final: "
-                    + "%.4f\n", 
+                    + "%.4f(m1:%.1f | (m2:%.1f)\n",
                     mensajeFinal,
-                    estudiantes[i],
-                    promediosFinales[i]);
+                    estudiantes[i].toLowerCase(),
+                    promediosFinales[i],
+                    promediosMateria1[i],
+                    promediosMateria2[i]);
         }
-        
+
         System.out.printf("%s\n", mensajeFinal);
-        
+
     }
 
 }
